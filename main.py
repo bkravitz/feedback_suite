d1a398bec75782b867904055d5d220f6959dd74c
Daniele Visioni <daniele.visioni@gmail.com>
2021-03-23 10:59:00 -0400

    Inserted ramp up

--- main.py
+++ main.py
@@ -1,36 +1,74 @@
-# Explicit feedback for climate modeling
-# Copyright (C) 2020  Ben Kravitz
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+Last login: Tue Mar 23 09:13:30 on console
 
-# Main setup script
-#
-# Written by Ben Kravitz (bkravitz@iu.edu or ben.kravitz.work@gmail.com)
-# Last updated 11 July 2019
-#
-# This script provides the main information about the run.  Note that everything
-# here is specific to an individual run.  Each run should have its own copy
-# of the feedback suite.
-#
-# This script is written in native python format.  Be careful with brackets [],
-# white space, and making sure everything that needs to be a string is actually
-# a string by putting it in quotes ''.  All lines beginning with # are comments.
-#
-# Things you need to specify:
-#
-################## GLOBAL SETUP PARAMETERS ##################
-#
+The default interactive shell is now zsh.
+To update your account to use zsh, please run `chsh -s /bin/zsh`.
+For more details, please visit https://support.apple.com/kb/HT208050.
+(base) Danieles-Air:~ danielevisioni$ ssh -X visioni@cheyenne.ucar.edu
+Token_Response: 
+Last login: Mon Mar 15 12:36:33 2021 from 67.249.80.19
+
+******************************************************************************
+*                  Welcome to Cheyenne - March 22,2021
+******************************************************************************
+            Today in the Daily Bulletin (dailyb.cisl.ucar.edu)
+    
+	- Systems downtime for week of March 22-26
+	- Upcoming changes to Casper services
+	- Python tutorial March 24: Matplotlib
+	- New Casper HTC nodes ready for PBS jobs
+	- Starting Casper Jobs with PBS Pro: March 30 tutorial
+	- April 6-7 XSEDE workshop: Big Data and Machine Learning
+
+HPSS Update:          193 days until HPSS will be decommissioned.
+Quick Start:          www2.cisl.ucar.edu/resources/cheyenne/quick-start-cheyenne
+User environment:     www2.cisl.ucar.edu/resources/cheyenne/user-environment
+Key module commands:  module list, module avail, module spider, module help
+CISL Help:            support.ucar.edu -- 303-497-2400
+--------------------------------------------------------------------------------
+
+visioni@cheyenne6:~> sodux geostrat
+If 'sodux' is not a typo you can use command-not-found to lookup the package that contains it, like this:
+    cnf sodux
+visioni@cheyenne6:~> sudox geostrat
+Token_Response: 
+geostrat@cheyenne6:/glade/u/home/visioni> cd /glade/work/geostrat/
+geostrat@cheyenne6:/glade/work/geostrat> cd cases/
+geostrat@cheyenne6:/glade/work/geostrat/cases> ls -ltr
+total 19
+-rw-r--r--  1 geostrat geostrat 13328 Mar  3 14:10 ALLVARIABLES_user_nl_cam
+drwxr-xr-x  9 geostrat geostrat  4096 Mar 12 10:57 b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001.OLD
+drwxr-xr-x  7 geostrat geostrat  4096 Mar 12 14:48 b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001-TSMLT-GAUSS-DEFAULT.t001
+drwxr-xr-x 10 geostrat geostrat  4096 Mar 22 13:56 b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001
+geostrat@cheyenne6:/glade/work/geostrat/cases> cd cd b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001
+bash: cd: cd: No such file or directory
+geostrat@cheyenne6:/glade/work/geostrat/cases> cd b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001
+geostrat@cheyenne6:/glade/work/geostrat/cases/b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001> cd controller/
+geostrat@cheyenne6:/glade/work/geostrat/cases/b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001/controller> ls -ltr
+total 65
+-rwxr-xr-x 1 geostrat geostrat  4590 Mar 22 12:42 commonroutines.py
+-rwxr-xr-x 1 geostrat geostrat  5373 Mar 22 12:42 PIcontrol.py
+-rwxr-xr-x 1 geostrat geostrat 13549 Mar 22 12:42 driver.py
+-rwxr-xr-x 1 geostrat geostrat  4146 Mar 22 12:55 main.py
+-rw-r--r-- 1 geostrat geostrat   346 Mar 22 13:56 ControlLog_b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001.txt
+geostrat@cheyenne6:/glade/work/geostrat/cases/b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001/controller> vim main.py 
+geostrat@cheyenne6:/glade/work/geostrat/cases/b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001/controller> vim PIcontrol.py 
+geostrat@cheyenne6:/glade/work/geostrat/cases/b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001/controller> vim PIcontrol.py 
+geostrat@cheyenne6:/glade/work/geostrat/cases/b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001/controller> vim ControlLog_b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001.txt 
+geostrat@cheyenne6:/glade/work/geostrat/cases/b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001/controller> ./main.py 
+Traceback (most recent call last):
+  File "./main.py", line 94, in <module>
+    execfile(maindir+"/driver.py")
+  File "/glade/work/geostrat/cases/b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001/controller/driver.py", line 357, in <module>
+    execfile(pathtocontrol)
+  File "/glade/work/geostrat/cases/b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001/controller/PIcontrol.py", line 84, in <module>
+    ramp_up = dt2/x
+NameError: name 'x' is not defined
+geostrat@cheyenne6:/glade/work/geostrat/cases/b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001/controller> vim PIcontrol.py 
+geostrat@cheyenne6:/glade/work/geostrat/cases/b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001/controller> ./main.py 
+ERROR: case.setup must be run prior to running preview_namelists
+geostrat@cheyenne6:/glade/work/geostrat/cases/b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001/controller> vim ControlLog_b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001.txt 
+geostrat@cheyenne6:/glade/work/geostrat/cases/b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001/controller> vim main.py 
+
 #   casepath = The path where this specific case is built and run.
 #
 #   maindir = The path where the control suite sits.
@@ -74,20 +112,8 @@
 
 runname='b.e21.BW.f09_g17.SSP245-TSMLT-GAUSS-DEFAULT.001'
 
-casepath='/glade/work/geostrat/'+runname
+casepath='/glade/work/geostrat/cases/'+runname
 maindir=casepath+'/controller'
 scratchdir='/glade/scratch/geostrat/'+runname+'/allthetemporarystuff/'
 frequency='1y'
-maxrest=1000
-
-pathtocontrol=maindir+'/PIcontrol.py'
-
-variables=['TREFHT']
-archivepaths=len(variables)*['/glade/scratch/geostrat/archive/'+runname+'/atm/hist'] # do NOT include a '/' at the end of each path
-varlocs=len(variables)*['cam.h0']
-
-###########################
-### CALLING MAIN SCRIPT ###
-###########################
-
-execfile(maindir+"/driver.py")
+"main.py" 94L, 4146C                                                                                                                                                                                                                    81,14         72%
